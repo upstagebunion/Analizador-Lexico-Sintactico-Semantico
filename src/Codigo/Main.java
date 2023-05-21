0833afa263203ee12fc3f3574d0bcaa5ac9613fb
Upstagebunion <fco.garcia.solis@gmail.com>
2023-05-21 15:33:53 -0600

    -little change on the directory path

--- src/Codigo/Main.java
+++ src/Codigo/Main.java
@@ -34,7 +34,7 @@
 package Codigo;
 public class Main {
     public static void main(String[] args) {
-        LectorArchivo la = new LectorArchivo("Analizador-Lexico-Sintactico-Semantico/src/Docs/programa.txt"); //se Lee el archivo que contiene el programa
+        LectorArchivo la = new LectorArchivo("src/Docs/programa.txt"); //se Lee el archivo que contiene el programa
         AnalizadorLexico al = la.al; //Se construye el analizador Léxico y se usa el que contiene el lector de archivo
         Display dSim = new Display(al.lsimbolos,"Tabla de Simbolos sin repeticion", false); //Se muestra la primer tabla de variables sin repetición
         Display dEr = new Display(al.lerrores, "Tabla de Errores", false); //Se muestra la tabla de errores
@@ -44,8 +44,8 @@ public class Main {
         Display simb = new Display(ts.simbolos, "Tabla de Simbolos", true); //Se muestra la tabla de simbolos
         AnalizadorSemantico as = new AnalizadorSemantico(ts.tokens, ts.simbolos); //Se construye y se ejecuta el analisis semántico y las verificaciones de tipos
         Posfija pf = new Posfija(ts.tokens);    //Genera el Código en notación PosFija
-        SavePf spf = new SavePf(pf.pf, "Analizador-Lexico-Sintactico-Semantico/src/Docs/posfija.txt"); //Guarda en código en notacion posfija
+        SavePf spf = new SavePf(pf.pf, "src/Docs/posfija.txt"); //Guarda en código en notacion posfija
         GenCodP gcp = new GenCodP(pf.pf); //Genera el codigo P usando la notacion posfija del código
-        SavePf scp = new SavePf(gcp.codigoP, "Analizador-Lexico-Sintactico-Semantico/src/Docs/codigoP.txt"); //Guarda el códigoP en u .txt
+        SavePf scp = new SavePf(gcp.codigoP, "src/Docs/codigoP.txt"); //Guarda el códigoP en u .txt
     }
 }
